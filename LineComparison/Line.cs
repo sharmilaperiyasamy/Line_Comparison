fd3ba443e8de875b4ee3952fbd0de68484d9d884
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-06 16:14:06 +0530

    [Sharmila] finding length

---
+++ LineComparison/Line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Line
+    {
+        public double finding_length(double x1, double x2, double y1, double y2)
+        {
+            double length = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            return length;
+        }
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,4 @@
+﻿LineComparison.Line line = new LineComparison.Line();
+
+double l1 = line.finding_length(5, 1, 3, 4);
+Console.WriteLine("Length of Line 1 is :" + l1);

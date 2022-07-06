a246781b2797cc8b30167262f8bfd07c86e21376
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-06 16:24:12 +0530

    [Sharmila] Comparing two lines

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,4 +6,16 @@ double l2 = line.finding_length(6, 4, 10, 2);
 Console.WriteLine("Length of Line 1 is :" + l1);
 Console.WriteLine("Length of Line 2 is :" + l2);
 
-Console.WriteLine(l1.Equals(l2));
+//Console.WriteLine(l1.Equals(l2));
+if (l1 > l2)
+{
+    Console.WriteLine("Line1 is greater than Line2.");
+}
+else if (l2 > l1)
+{
+    Console.WriteLine("Line2 is greater than Line1.");
+}
+else
+{
+    Console.WriteLine("Both Line1 and line2 are equal");
+}

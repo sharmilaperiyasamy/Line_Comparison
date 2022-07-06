cf995141156a1ff3541fb258bc4146e23aa0872d
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-06 16:20:16 +0530

    [Sharmila] Checking equality

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,4 +1,9 @@
 ﻿LineComparison.Line line = new LineComparison.Line();
 
 double l1 = line.finding_length(5, 1, 3, 4);
+double l2 = line.finding_length(6, 4, 10, 2);
+
 Console.WriteLine("Length of Line 1 is :" + l1);
+Console.WriteLine("Length of Line 2 is :" + l2);
+
+Console.WriteLine(l1.Equals(l2));
